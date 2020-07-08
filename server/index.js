e92d66a5a435099989e481dbd86d6f782a3670b5
richardgohcr <richard.gohcr@gmail.com>
2020-07-08 17:59:08 +0800

    Addeed docker compose

--- server/index.js
+++ server/index.js
@@ -21,11 +21,11 @@ const pgClient = new Pool({
   port: keys.pgPort,
 });
 
-pgClient.on('error', () => console.log('Lost pg connection'));
-
-pgClient
-  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch((err) => console.log(err));
+pgClient.on('connect', () => {
+  pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.log(err));
+});
 
 //Redis Client Setup
 const redis = require('redis');
--- worker/index.js
+++ worker/index.js
@@ -1,11 +1,11 @@
 const keys = require('./keys');
 const redis = require('redis');
 
-const redisClient = (redis = createClient({
+const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
   retry_strategy: () => 1000,
-}));
+});
 
 const sub = redisClient.duplicate();
 
