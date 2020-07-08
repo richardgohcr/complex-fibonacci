3b157a289d3d47964a0ce3bfe5a9dc2c88d3d76e
richardgohcr <richard.gohcr@gmail.com>
2020-07-08 11:19:29 +0800

    Connecting to postgress

--- server/index.js
+++ server/index.js
@@ -11,3 +11,19 @@ const app = express();
 app.use(cors());
 app.unsubscribe(bodyParser.json());
 
+//Postgres Client Setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.password,
+  port: keys.pgPort,
+});
+
+pgClient.on('error', () => console.log('Lost pg connection'));
+
+pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+  .catch((err) => console.log(err));
+
